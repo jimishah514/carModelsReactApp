a56cdab453e597ef3c6678aa16128b7f2a169124
jimi <jamalmoosvi@gmail.com>
2021-11-06 17:45:52 +0500

    removed commented code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import * as api from './api.service/api'
 
 function App() {
-  const [userData, setUserData] = useState({});
+  const [carModels, setCarModels] = useState({});
 
   useEffect(() => {
     fetchCarModels();
@@ -12,22 +12,22 @@ function App() {
   const fetchCarModels = async () => {
     const carList = await api.getCarModels()
     console.log("List : ", carList)
-    setUserData(carList)
+    setCarModels(carList)
   }
-console.log("userData",userData)
-console.log("type userData",typeof userData)
-const userDataArray = Object.values(userData)
-console.log("userDataArray",userDataArray)
-console.log("type userDataArray",typeof userDataArray)
-// userDataArray.map(d => {
-//   console.log("###",d.automobile)
-// })
+  const userDataArray = Object.values(carModels)
   return (
     <>
-     {/* <h5 >{userData[0] && userData[0].automobile}</h5> */}
-      {userDataArray.map((obj) => (
-        <p key={obj.automobile}>Automobile: {obj.automobile} , Production: {obj.production} .</p>
-      ))}
+      <ul className="list-group">
+        {userDataArray.map((obj) => (
+          <li key={obj.automobile} className="list-group-item">Automobile: {obj.automobile} , Production: {obj.production} .</li>
+        ))}
+        {/* {this.state.listitems.map(listitem => (
+          <li key={listitem.id} className={listitem.modifier}>
+            {listitem.context}
+          </li>
+        ))} */}
+      </ul>
+
     </>
   );
 }
